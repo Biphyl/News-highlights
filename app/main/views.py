f8efa0121ad288d30d2c90d8c0ebce614483c298
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-27 12:19:04 +0300

    imported Sources and Articles from models

--- app/main/views.py
+++ app/main/views.py
@@ -7,6 +7,9 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+
+    title = 'News Highlights'
+    
     return render_template('index.html')
 
 @main.route('/articles/<id>')
---
+++ app/requests.py
@@ -0,0 +1,4 @@
+from .models import Sources,Articles
+
+# Getting the api key
+api_key = None
