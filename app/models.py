c51f26527ab0d4c5b492afc3d9eba0e027356d02
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-27 12:31:37 +0300

    added class Sources in the models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,15 @@
-
+class Sources:
+    '''
+    Sources class to define Sources Objects
+    '''
+    def __init__(self,id,name,description,url,category,country,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+        self.language = language
 class Articles:
     '''
     Articles class to define articles objects
