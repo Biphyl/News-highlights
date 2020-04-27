e1267f230ec878eb429d5d48f08b0a83978bf171
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-04-27 11:58:36 +0300

    created articles page view function

--- app/main/views.py
+++ app/main/views.py
@@ -8,3 +8,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+
+@main.route('/articles/<id>')
+def articles(id):
+    '''
+    View source page function that returns the source details page and its data
+    '''
+    return render_template('articles.html')
